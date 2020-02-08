3c85a1264a762438362e42047c1f671d78550ca7
Ken-Williams OYE <k.williams.oye@gmail.com>
2020-02-08 15:31:29 +0100

    Display posts

--- app/src/main/java/com/jdemandre/instartist/Fragments/HomeFragment.java
+++ app/src/main/java/com/jdemandre/instartist/Fragments/HomeFragment.java
@@ -4,29 +4,52 @@ package com.jdemandre.instartist.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jdemandre.instartist.Adapters.PostAdapter;
+import com.jdemandre.instartist.Model.Post;
 import com.jdemandre.instartist.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class HomeFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+    private List<Post> postList;
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
+    private List<String> followingList;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        mLayoutManager.setReverseLayout(true);
+        mLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(mLayoutManager);
+        postList = new ArrayList<>();
+        postAdapter = new PostAdapter(getContext(), postList);
+        recyclerView.setAdapter(postAdapter);
+
+
+        return view;
+    }
+
+    private void readPost(){
+        // TODO
     }
 
 }
