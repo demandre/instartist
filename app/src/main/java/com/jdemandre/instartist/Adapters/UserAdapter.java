671aca652536942013ff90483555f66d256b4d6f
Ken-Williams OYE <k.williams.oye@gmail.com>
2020-02-08 12:44:00 +0100

    Search view

---
+++ app/src/main/java/com/jdemandre/instartist/Adapters/UserAdapter.java
@@ -0,0 +1,40 @@
+package com.jdemandre.instartist.Adapters;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jdemandre.instartist.Model.User;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter {
+
+    private Context mContext;
+    private List<User> mUsers;
+    private boolean isFragment;
+
+    public UserAdapter(Context context, List<User> users, boolean isFragment){
+        mContext = context;
+        mUsers = users;
+        this.isFragment = isFragment;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/jdemandre/instartist/Fragments/SearchFragment.java
+++ app/src/main/java/com/jdemandre/instartist/Fragments/SearchFragment.java
@@ -4,17 +4,28 @@ package com.jdemandre.instartist.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.jdemandre.instartist.Adapters.UserAdapter;
+import com.jdemandre.instartist.Model.User;
 import com.jdemandre.instartist.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private UserAdapter userAdapter;
+    private List<User> userList;
+
+    EditText search_bar;
 
 
     public SearchFragment() {
@@ -25,8 +36,21 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        search_bar = view.findViewById(R.id.search_bar);
+
+        userList = new ArrayList<>();
+        userAdapter = new UserAdapter(getContext(), userList, true);
+        recyclerView.setAdapter(userAdapter);
+
+
+        return view;
     }
 
 }
