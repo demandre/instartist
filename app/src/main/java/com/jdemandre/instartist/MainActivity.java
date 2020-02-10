e5d2d75b6864acd554d2bc19d459e8ec58791a2f
Joris Demandre <joris.demandre@agence-soon.fr>
2020-02-10 14:29:45 +0100

    clean and finalise merge

--- app/src/main/java/com/jdemandre/instartist/MainActivity.java
+++ app/src/main/java/com/jdemandre/instartist/MainActivity.java
@@ -1,13 +1,12 @@
 package com.jdemandre.instartist;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.jdemandre.instartist.Fragments.HomeFragment;
--- app/src/main/java/com/jdemandre/instartist/StartActivity.java
+++ app/src/main/java/com/jdemandre/instartist/StartActivity.java
@@ -1,14 +1,15 @@
 package com.jdemandre.instartist;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -16,17 +17,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.jdemandre.instartist.Fragments.HomeFragment;
-import com.jdemandre.instartist.Fragments.NotificationFragment;
-import com.jdemandre.instartist.Fragments.ProfileFragment;
-import com.jdemandre.instartist.Fragments.SearchFragment;
 import com.squareup.picasso.Picasso;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -103,7 +94,6 @@ public class StartActivity extends AppCompatActivity {
             } else {
                 Picasso.get().load("https://kooledge.com/assets/default_medium_avatar-57d58da4fc778fbd688dcbc4cbc47e14ac79839a9801187e42a796cbd6569847.png").into(imageView);
             }
-        } else {
             startActivity(new Intent(StartActivity.this, MainActivity.class));
         }
     }
