f352203b9c8b31dbcb6fc06f811f93283fa73a9f
Joris Demandre <joris.demandre@agence-soon.fr>
2020-02-10 13:18:32 +0100

    add admob use on mainactivity for example

--- app/src/main/java/com/jdemandre/instartist/MainActivity.java
+++ app/src/main/java/com/jdemandre/instartist/MainActivity.java
@@ -3,10 +3,23 @@ package com.jdemandre.instartist;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdCallback;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -15,13 +28,31 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 import com.squareup.picasso.Picasso;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE = 1;
     private GoogleSignInClient client;
+
+    private RewardedAd rewardedAd;
+
+    public RewardedAd createAndLoadRewardedAd() {
+        RewardedAd rewardedAd = new RewardedAd(this,
+                "ca-app-pub-3940256099942544/5224354917");
+        RewardedAdLoadCallback adLoadCallback = new RewardedAdLoadCallback() {
+            @Override
+            public void onRewardedAdLoaded() {
+                Toast.makeText(MainActivity.this, "New ad loaded", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRewardedAdFailedToLoad(int errorCode) {
+                Toast.makeText(MainActivity.this, "New ad failed loaded + errcode " + errorCode, Toast.LENGTH_SHORT).show();
+            }
+        };
+        rewardedAd.loadAd(new AdRequest.Builder().build(), adLoadCallback);
+        return rewardedAd;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +69,50 @@ public class MainActivity extends AppCompatActivity {
                 signIn();
             }
         });
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        rewardedAd = createAndLoadRewardedAd();
+
+
+        findViewById(R.id.adButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (rewardedAd.isLoaded()) {
+                    RewardedAdCallback adCallback = new RewardedAdCallback() {
+                        @Override
+                        public void onRewardedAdOpened() {
+                            Toast.makeText(MainActivity.this, "New ad opened", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onRewardedAdClosed() {
+                            MainActivity.this.rewardedAd = createAndLoadRewardedAd();
+                        }
+
+                        @Override
+                        public void onUserEarnedReward(@NonNull RewardItem reward) {
+                            Toast.makeText(MainActivity.this, "You win !!!", Toast.LENGTH_SHORT).show();
+                            Log.d("jo", String.valueOf(reward.getAmount()));
+                            // TODO: handle give reward to the user we want
+                        }
+
+                        @Override
+                        public void onRewardedAdFailedToShow(int errorCode) {
+                            Toast.makeText(MainActivity.this, "New ad failed to display: errcode: " + errorCode, Toast.LENGTH_SHORT).show();
+                        }
+                    };
+                    rewardedAd.show(MainActivity.this, adCallback);
+                } else {
+                    Toast.makeText(MainActivity.this, "Please wait, ad is loading...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
     private void signIn() {
