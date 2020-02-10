c38f066b336bb930b2d939fd68ffc11ed87abbeb
Ken-Williams OYE <k.williams.oye@gmail.com>
2020-02-10 13:27:00 +0100

    Always connected

--- app/src/main/java/com/jdemandre/instartist/StartActivity.java
+++ app/src/main/java/com/jdemandre/instartist/StartActivity.java
@@ -103,7 +103,7 @@ public class StartActivity extends AppCompatActivity {
             } else {
                 Picasso.get().load("https://kooledge.com/assets/default_medium_avatar-57d58da4fc778fbd688dcbc4cbc47e14ac79839a9801187e42a796cbd6569847.png").into(imageView);
             }
-
+        } else {
             startActivity(new Intent(StartActivity.this, MainActivity.class));
         }
     }
